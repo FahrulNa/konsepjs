8f1a93f1c5474316042c7e17d3e35f3513a49779
FahrulNa <140799150+FahrulNa@users.noreply.github.com>
2023-10-11 09:52:33 +0700

    Add files via upload

---
+++ biodata.js
@@ -0,0 +1,13 @@
+let nama="Fahrul Nabawi"
+let age= 16
+let classes="XI PPLG 3"
+let addres="gunungsalem"
+let email="fahrulnabawia@gmail.com"
+
+document.getElementById("biodata").innerHTML =
+"BIODATA" +
+"<br>Nama : "+ nama +
+"<br>Umur : "+ age +
+"<br>Kelas : "+ classes +
+"<br>Alamat : "+ addres +
+"<br>Email : "+ email ;
---
+++ perhitungan.js
@@ -0,0 +1,7 @@
+let x = 2
+let y = 68
+
+console.log("penjumlahan :"+ (x+y));
+console.log("pengurangan :"+ (x-y));
+console.log("perkalian :"+ (x*y));
+console.log("pembagian :"+ (x/y))
